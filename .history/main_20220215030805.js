0cec28147c1fb87a2f281fc3f31eb9ca314480b7
fauzaanmhmd <fauzanmuh313@gmail.com>
2022-02-28 18:03:50 +0700

    first commit

---
+++ .history/main_20220215030805.js
@@ -0,0 +1,56 @@
+// Book Class: Represents a Book
+
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+    static displayBooks() {
+        const StoreBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '344312'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '142541'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookList());
+    }
+
+    static addBookToList() {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+// Store Class: Handle Storage
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Event: Add a Book
+
+// Event: Remove a Book
